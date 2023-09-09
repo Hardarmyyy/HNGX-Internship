ee2e17ecbf2eca8d1c8c2832b7ef340ecbf5b588
hardarmyyy <adeyemoadams005@gmail.com>
2023-09-09 15:50:10 +0100

    FIX:utc time format

--- BACK-END/Stage-One/index.js
+++ BACK-END/Stage-One/index.js
@@ -9,21 +9,23 @@ app.use((req, res, next) => {
     next()
 })
 
-// Define an array to store all the days of the week
-const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-
-// Define and store the current date in the today Variable
-const today = new Date()
-
-// Define and store the index of the day in the currentDay variable 
-const currentDay = today.getDay()
-
-const currentDayofTheWeek = daysOfTheWeek[currentDay]
-
-const UTCTime = (today.toISOString()).slice(0, 19) + 'Z'
 
 app.get('/api', (req, res) => {
+        // Define an array to store all the days of the week
+        const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+        // Define and store the current date in the today Variable
+        const today = new Date()
+
+        // Define and store the index of the day in the currentDay variable 
+        const currentDay = today.getDay()
+
+        const currentDayofTheWeek = daysOfTheWeek[currentDay]
+
+        const UTCTime = today.toISOString().split('.')[0] + 'Z' 
+        
         const {slack_name, track} = req.query
+
     try {
         if (slack_name === 'Hardarmyyy' && track === 'backend') {
             res.status(200).json({
