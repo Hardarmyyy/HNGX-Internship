bc076f74978efe8d63531c0995d83065097a72f7
hardarmyyy <adeyemoadams005@gmail.com>
2023-09-12 12:41:56 +0100

    connect to db

--- BACK-END/Stage-One/index.js
+++ BACK-END/Stage-One/index.js
@@ -1,55 +1,64 @@
+const mongoose = require('mongoose')
 const express = require('express');
 const app = express();
 require('dotenv').config()
 
-app.use(express.json()) 
+mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true})
+.then(() => {
+    app.use(express.json()) 
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+    app.use((req, res, next) => {
+        console.log(req.path, req.method)
+        next()
+    })
 
 
-app.get('/api', (req, res) => {
-        // Define an array to store all the days of the week
-        const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    app.get('/api', (req, res) => {
+            // Define an array to store all the days of the week
+            const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-        // Define and store the current date in the today Variable
-        const today = new Date()
+            // Define and store the current date in the today Variable
+            const today = new Date()
 
-        // Define and store the index of the day in the currentDay variable 
-        const currentDay = today.getDay()
+            // Define and store the index of the day in the currentDay variable 
+            const currentDay = today.getDay()
 
-        const currentDayofTheWeek = daysOfTheWeek[currentDay]
+            const currentDayofTheWeek = daysOfTheWeek[currentDay]
 
-        const UTCTime = today.toISOString().split('.')[0] + 'Z' 
-        
-        const {slack_name, track} = req.query
+            const UTCTime = today.toISOString().split('.')[0] + 'Z' 
+            
+            const {slack_name, track} = req.query
 
-    try {
-        if (slack_name === 'Hardarmyyy' && track === 'backend') {
-            res.status(200).json({
-                "slack_name": "Hardarmyyy",
-                "current_day": currentDayofTheWeek,
-                "utc_time": UTCTime,
-                "track": "backend",
-                "github_file_url": "https://github.com/Hardarmyyy/HNGX-Internship/blob/main/BACK-END/Stage-One/index.js",
-                "github_repo_url": "https://github.com/Hardarmyyy/HNGX-Internship/tree/main/BACK-END/Stage-One",
-                "status_code": 200,
-            })
+        try {
+            if (slack_name === 'Hardarmyyy' && track === 'backend') {
+                res.status(200).json({
+                    "slack_name": "Hardarmyyy",
+                    "current_day": currentDayofTheWeek,
+                    "utc_time": UTCTime,
+                    "track": "backend",
+                    "github_file_url": "https://github.com/Hardarmyyy/HNGX-Internship/blob/main/BACK-END/Stage-One/index.js",
+                    "github_repo_url": "https://github.com/Hardarmyyy/HNGX-Internship/tree/main/BACK-END/Stage-One",
+                    "status_code": 200,
+                })
+            }
+            else {
+                res.status(404).send({message: 'The username or track is invalid'})
+            }
         }
-        else {
-            res.status(404).send({message: 'The username or track is invalid'})
+        catch (error) {
+            res.status(500).send({message: error.message})
         }
-    }
-    catch (error) {
-        res.status(500).send({message: error.message})
-    }
+    })
+
+    app.listen(process.env.PORT, () => {
+        console.log('connected to db and listening and running on port', process.env.PORT)
+    })
+})
+.catch((err) => {
+    console.log(err)  
 })
 
-app.listen(process.env.PORT, () => {
-    console.log('server is listening and running on port', process.env.PORT)
-})
+
 
 // Export the Express API for vercel build up process
 module.exports = app;
