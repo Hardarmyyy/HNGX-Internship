6e75ae36ba4780960e96e507b8641e3bef55ec78
hardarmyyy <adeyemoadams005@gmail.com>
2023-09-12 03:29:18 +0100

    FIX: vercel file

--- BACK-END/Stage-Two/index.js
+++ BACK-END/Stage-Two/index.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-
-const userRoutes = require('./routes/userRoutes')
+const User = require('./models/user')
 
 //connect to database
 mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true})  
@@ -17,7 +16,98 @@ mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlPars
         next()
     })
 
-    app.use('/api', userRoutes)
+    app.post('/api', async (req, res, next) => {
+
+        const {name, track} = req.body
+    
+        try {
+            if (!name || !track) {
+                return res.status(404).send({error: 'All fields are required'})
+            }
+            else {
+                const user = new User({name: name, track: track})
+                await user.save()
+                res.status(200).send({message: 'user added successfully', user: user})
+            }
+        }
+        catch (err) {
+            res.status(500).send({error: err.message});
+        }
+    })
+
+    app.get('/api/:user_id', async (req, res, next) => {
+        const {user_id} = req.params
+    
+        try {
+            if (!mongoose.Types.ObjectId.isValid(user_id)) {
+                return res.status(404).json({ error: "The user ID is invalid!" })
+            }
+    
+            const user = await User.findById({_id: user_id})
+            if (!user) {
+                return res.status(404).send({error: 'User not found'});
+            }
+    
+            res.status(200).send({user: user});
+    
+        }
+        catch (err) {
+            res.status(500).send({error: err.message});
+        }
+        
+    })
+
+    app.patch('/api/:user_id', async (req, res, next) => {
+        const {user_id} = req.params
+
+        try {
+            if (!mongoose.Types.ObjectId.isValid(user_id)) {
+                return res.status(404).json({ error: "The user ID is invalid!" })
+            }
+
+            const user = await User.findOne({_id: user_id})
+            if (!user) {
+                return res.status(404).send({error: 'User not found'});
+            }
+
+            const {name, track} = req.body;
+            if (!name || !track) {
+                return res.status(404).send({error: 'All fields are required'});
+            }
+            user.name = name;
+            user.track = track;
+            await user.save();
+
+            res.status(200).send({message: 'user updated successfully', user: user});
+
+        }
+        catch (err) {
+            res.status(500).send({error: err.message});
+        }
+    
+    })
+
+    app.delete('/api/:user_id', async (req, res, next) => {
+        const {user_id} = req.params
+    
+        try {
+            if (!mongoose.Types.ObjectId.isValid(user_id)) {
+                return res.status(404).json({ error: "The user ID is invalid!" })
+            }
+    
+            const user = await User.findOneAndDelete({_id: user_id})
+            if (!user) {
+                return res.status(404).send({error: 'User not found'});
+            }
+    
+            res.status(200).send({message: 'user deleted successfully'});
+    
+        }
+        catch (err) {
+            res.status(500).send({error: err.message});
+        }
+        
+    })
 
     //listen for requests
     app.listen(process.env.PORT, () => {
