912d0c73a6b97d20114e77e834d04758979641e6
hardarmyyy <adeyemoadams005@gmail.com>
2023-09-12 03:14:22 +0100

    Stage-Two-Backend

---
+++ BACK-END/Stage-Two/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const userRoutes = require('./routes/userRoutes')
+
+//connect to database
+mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true})  
+.then(() => {
+
+    // Middlewares
+    app.use(express.json())
+
+    app.use((req, res, next) => {
+        console.log(req.path, req.method)  
+        next()
+    })
+
+    app.use('/api', userRoutes)
+
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log('Connected to Database and Server is listening and running on port ' + process.env.PORT)
+    })
+})
+.catch((err) => {
+        console.log(err)  
+}) 
+
+// Export the Express API for vercel build up process
+module.exports = app;
