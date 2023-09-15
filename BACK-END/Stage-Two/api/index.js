42608c1914f33e5a4bfcf3b681eeed8b0b2bd448
hardarmyyy <adeyemoadams005@gmail.com>
2023-09-15 09:47:31 +0100

    vercel file

--- BACK-END/Stage-Two/api/index.js
+++ BACK-END/Stage-Two/api/index.js
@@ -15,30 +15,6 @@ mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlPa
 
     app.use(cors())
 
-    const whitelist = [
-        '*'
-    ];
-    
-    app.use((req, res, next) => {
-    const origin = req.get('referer');
-    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-    if (isWhitelisted) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-    }
-    // Pass to next layer of middleware
-    if (req.method === 'OPTIONS') res.sendStatus(200);
-    else next();
-    });
-    
-    const setContext = (req, res, next) => {
-    if (!req.context) req.context = {};
-    next();
-    };
-    app.use(setContext);
-
     app.use((req, res, next) => {
         console.log(req.path, req.method) 
         next()
